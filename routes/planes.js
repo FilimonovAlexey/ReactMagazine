21b2b2dd84918577a3a6d4963216474a40904a51
Alexey Filimonov <alexeyf08@gmail.com>
2023-04-20 02:13:34 +0700

    Добавлен контроллер

--- routes/planes.js
+++ routes/planes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { getPlanes } = require('../controllers/planes');
 // const path = require('patch');
 
-router.get('/', (req, res) => res.send('get all planes'))
+//Получить все самолеты
+router.get('/', getPlanes)
+//Получить самолет по id
 router.get('/:id', (req, res) => res.send('get single planes'))
+//Создать самолет
 router.post('/', (req, res) => res.send('create planes'))
 
 module.exports = router;
