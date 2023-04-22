ddf15cc632ed68a9c1f91ff39c72785d1dc38edc
Alexey Filimonov <alexeyf08@gmail.com>
2023-04-22 16:46:31 +0700

    + валидация на сервере при вводе данных

--- controllers/planes.js
+++ controllers/planes.js
@@ -14,6 +14,40 @@ const getPlanes = async (req, res) => {
 }
 
 const createPlanes = async (req, res) => {
+  const errors = {};
+
+  if (!req.body.name) {
+    errors.name = { message: "Укажите имя" }
+  }
+
+  if (!req.body.price) {
+    errors.price = { message: "Укажите цену" }
+  }
+
+  if (!req.body.description) {
+    errors.description = { message: "Укажите описание" }
+  }
+
+  if (req.body.description && req.body.description.length > 700) {
+    errors.description = { message: "Описание очень длинное" }
+  }
+
+  if (!req.body.capacity) {
+    errors.capacity = { message: "Укажите вместимость" }
+  }
+
+  if (req.body.capacity && req.body.capacity.length > 2) {
+    errors.capacity = { message: "Вместимость не может быть больше 99" }
+  }
+
+  if (!req.file) {
+    errors.planeImage = { message: "Добавьте фото" }
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
   try {
     const { name, price, description, capacity } = req.body;
     const plane = await Plane.create({
