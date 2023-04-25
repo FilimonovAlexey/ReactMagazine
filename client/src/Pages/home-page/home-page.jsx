778c7a1c3ac553180ca967ecd967fde98203e514
Alexey Filimonov <alexeyf08@gmail.com>
2023-04-25 21:18:14 +0700

    Добавлен компонент Planes

--- client/src/Pages/home-page/home-page.jsx
+++ client/src/Pages/home-page/home-page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Header } from "../../components/header/header"
+import { Header } from "../../components/header"
+import { Planes } from "../../components/planes";
 
 export const Homepage = () => {
   return (
     <React.Fragment>
       <Header />
+      <Planes />
     </React.Fragment>
   )
 }
