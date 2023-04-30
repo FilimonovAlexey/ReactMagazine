956ca3c7bf9781d144362c5b6d3bae782aeefc0a
Alexey Filimonov <alexeyf08@gmail.com>
2023-04-30 23:20:36 +0700

    Запрос на сервер из компонента planes

--- client/src/components/planes/planes.jsx
+++ client/src/components/planes/planes.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPlanes } from '../../store/planes/planesSlice'
 
 export const Planes = () => {
-  return (
-    <div>Planes</div>
-  )
+  const dispatch = useDispatch();
+  const { planes, isLoading } = useSelector((state) => state.planes);
+
+  useEffect(() => {
+    dispatch(getPlanes())
+  }, [dispatch])
+
+  return <div>Planes</div>;
 }
